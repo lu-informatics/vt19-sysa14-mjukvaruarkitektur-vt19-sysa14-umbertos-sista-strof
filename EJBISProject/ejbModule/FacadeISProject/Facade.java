ff620b62073e0854e60708ccad7dbfd83a413a17
Jonaswerne <43535592+Jonaswerne@users.noreply.github.com>
2019-04-29 15:54:14 +0200

    Update Facade.java

--- EJBISProject/ejbModule/FacadeISProject/Facade.java
+++ EJBISProject/ejbModule/FacadeISProject/Facade.java
@@ -1,20 +1,59 @@
 package FacadeISProject;
 
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import EAOISProject.BookLocal;
+import EAOISProject.LoaningImpl;
+import EAOISProject.LoaningLocal;
+import EAOISProject.ReserveLocal;
+import ejbModule.Book;
+import ejbModule.BookID;
+import ejbModule.Loaning;
+import ejbModule.Reserve;
 
 /**
  * Session Bean implementation class Facade
  */
+
 @Stateless
 @LocalBean
 public class Facade implements FacadeRemote, FacadeLocal {
+	
+	@EJB
+	LoaningLocal Loaning;
+	@EJB
+	ReserveLocal Reserve;
+	@EJB
+	BookLocal Book;
 
     /**
      * Default constructor. 
      */
     public Facade() {
-        // TODO Auto-generated constructor stub
+    	
     }
+    public List<Loaning> PersonLoans(String id){
+    	return Loaning.PersonLoans(id);
+    }
+    public Reserve CreateReserve(Reserve reserve) {
+    	return Reserve.CreateReserve(reserve);
+    }
+	public List<Reserve> PersonReserve(String id){
+		return Reserve.PersonReserve(id);
+	}
+	public Book findBook(BookID bookID) {
+		return Book.findBook(bookID);
+	}
+	public List<Book> SearchBook(String searchTerm){
+		return Book.SearchBook(searchTerm);
+	}
+	public List<Book> FindAllBooks(){
+		return Book.FindAllBooks();
+	}
+    
 
 }
