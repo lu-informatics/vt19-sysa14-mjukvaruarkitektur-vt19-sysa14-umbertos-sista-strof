a2f79c2043361dff1c1445f28bc96066aac4a741
Jonaswerne <43535592+Jonaswerne@users.noreply.github.com>
2019-05-05 13:52:57 +0200

    rip

--- WebISProject/src/Servlets/ServletISProject.java
+++ WebISProject/src/Servlets/ServletISProject.java
@@ -29,12 +29,6 @@ public class ServletISProject extends HttpServlet {
 
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE html><html><head>");
-		out.println("<title>Test</title>");
-		out.println("<meta charset=\"ISO-8859-1\">");
-		out.println("</head><body>");
-		out.println("<h1>Test</h1>");
-		out.println("</body></html>");
     }
 
 }
