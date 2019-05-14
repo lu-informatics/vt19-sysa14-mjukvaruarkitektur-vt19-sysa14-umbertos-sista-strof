4cfd8e95100dcee44dae3d5ad05a67f94eb64403
Jonaswerne <43535592+Jonaswerne@users.noreply.github.com>
2019-05-14 16:08:43 +0200

    Create PersonEJBTest.java

---
+++ WebISProject/src/Test/PersonEJBTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import junit.framework.TestCase;
+
+public class PersonEJBTest extends TestCase {
+
+	public PersonEJBTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
